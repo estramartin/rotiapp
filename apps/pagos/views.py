7a773f1330a8586757a6e624f8d7354da613ab3f
estramartin <estramartin@gmail.com>
2024-07-08 21:46:00 -0300

    fix action post webhook

--- apps/pagos/views.py
+++ apps/pagos/views.py
@@ -1,11 +1,13 @@
 import logging
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
 
 class PagoViewSet(viewsets.ViewSet):
-
+    
+    @action(detail=False, methods=['post'])
     def webhook(self, request):
         logger.info(f'Webhook received. {request.data}')
         return Response({'status': 'ok'})
