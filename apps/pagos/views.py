c78a55f5025601d0869149eeb1b7fedd4b2e560b
estramartin <estramartin@gmail.com>
2024-07-08 21:48:59 -0300

    AllowAny para webhook

--- apps/pagos/views.py
+++ apps/pagos/views.py
@@ -1,12 +1,13 @@
 import logging
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
 
 class PagoViewSet(viewsets.ViewSet):
-    
+    permission_classes = [AllowAny]
     @action(detail=False, methods=['post'])
     def webhook(self, request):
         logger.info(f'Webhook received. {request.data}')
